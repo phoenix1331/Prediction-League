5ece8366adad3156e53b9c075b8976bafe15bf79
Darren Williams <design@phoenix1331.co.uk>
2015-02-18 20:48:01 +0000

    Tidy form and add warning message

--- application/views/head.php
+++ application/views/head.php
@@ -12,7 +12,7 @@
     <title>Prediction League</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/bootstrap.css'); ?>" rel="stylesheet">
      <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
 
 
