075890738227964043562e301fddff7ddb23ea44
Darren Williams <design@phoenix1331.co.uk>
2015-02-20 21:09:28 +0000

    added footer

--- application/views/foot.php
+++ application/views/foot.php
@@ -1,3 +1,8 @@
+</div> <!-- close main container -->
+<footer class="clearfix">
+	<div class="container"><p>&copy Darren Williams 2015</p></div>
+</footer>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
   </body>
