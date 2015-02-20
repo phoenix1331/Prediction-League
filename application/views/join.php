62e4335d80353d5294d432cfd113132c1400e1b9
Darren Williams <design@phoenix1331.co.uk>
2015-02-20 20:46:06 +0000

    Join form edited

--- application/views/join.php
+++ application/views/join.php
@@ -17,7 +17,19 @@
               'name' => 'password',
               'id' => 'password',
               'placeholder' => 'Password',
+              'value' => ''),
+                    'passwordAgain' => array(
+              'name' => 'passwordAgain',
+              'id' => 'passwordAgain',
+              'placeholder' => 'Password Again',
+              'value' => ''),
+              'email' => array(
+              'name' => 'email',
+              'id' => 'email',
+              'placeholder' => 'email',
               'value' => '')
+
+
         )
     ?>
      <?php 
@@ -31,6 +43,8 @@
         <?php echo form_open('login/check',array('class'=>'form-control')) ?>
         <?php echo form_input($form['username']); ?>
         <?php echo form_input($form['password']); ?>
+        <?php echo form_input($form['passwordAgain']); ?>
+        <?php echo form_input($form['email']); ?>
         <?php echo form_submit(array('name'=>'submit', 'class'=>'submit'), 'Submit'); ?>
         <?php echo form_close(); ?>
       </p>
