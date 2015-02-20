96ce2c2ef7d57a98b0412a932603b9fececfe85d
Darren Williams <design@phoenix1331.co.uk>
2015-02-20 14:05:27 +0000

    Form edits

--- application/views/join.php
+++ application/views/join.php
@@ -5,19 +5,24 @@
           'username' => array(
               'name' => 'username',
               'id' => 'username',
+              'placeholder' => 'Username',
+              'autofocus'   => 'autofocus',
               'value' => ''),
           'password' => array(
               'name' => 'password',
               'id' => 'password',
+              'placeholder' => 'Password',
               'value' => '')
         )
     ?>
-      <p>
+     <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <?php echo $message; ?>
-        <?php echo form_open('join/check') ?>
-        <label for="username">Username <span class="required">*</span></label>
+    </div>
+  
+      <p>
+        <?php echo form_open('login/check',array('class'=>'form-control')) ?>
         <?php echo form_input($form['username']); ?>
-        <label for="password">Password <span class="required">*</span></label>
         <?php echo form_input($form['password']); ?>
         <?php echo form_submit(array('name'=>'submit', 'class'=>'submit'), 'Submit'); ?>
         <?php echo form_close(); ?>
