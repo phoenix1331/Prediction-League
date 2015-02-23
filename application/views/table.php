904977ebe7aa9ca5ff5463ee0f841c0ac8823254
Darren Williams <design@phoenix1331.co.uk>
2015-02-23 18:44:15 +0000

    Table edits

--- application/views/table.php
+++ application/views/table.php
@@ -5,18 +5,53 @@
 </div>
 <div class="container main">
 
+<table class="mainTable">
+  <thead>
+    <tr>
+      <th>Pos</th>
+      <th class="name">Name</th>
+      <th>P</th>
+      <th>W</th>
+      <th>D</th>
+      <th>L</th>
+      <th>F</th>
+      <th>A</th>
+      <th>GD</th>
+      <th>Pts</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td class="name">Darren Williams</td>
+      <td>6</td>
+      <td>5</td>
+      <td>1</td>
+      <td>0</td>
+      <td>24</td>
+      <td>21</td>
+      <td>3</td>
+      <td>34</td>
+      <td><</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td></td>
+      <td class="name"></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tfoot>
+</table>
 
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-       Sed rerum deleniti, quidem tempora quaerat ad quae porro doloribus molestiae, 
-       ducimus nesciunt, sunt dolorum, laboriosam unde modi sint! Repudiandae, excepturi,
-        eaque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusamus 
-        iste dolor debitis ea, earum vero nam quasi fuga error nulla! Harum quis temporibus 
-        doloremque cupiditate, quisquam deserunt error accusantium eaque sed recusandae ipsa 
-        inventore repellat quo repudiandae eos nisi eius, eligendi assumenda dolore voluptatum 
-        itaque cumque architecto commodi? Earum necessitatibus quae, at. Tempore, quia, deserunt! 
-        Eos odit itaque doloremque temporibus nesciunt reiciendis. Reiciendis veniam, aliquam fugiat 
-        animi inventore sint laboriosam? Quae enim, aliquid fugit quasi ab ullam, quo porro. 
-        Blanditiis debitis ducimus fugit voluptatum suscipit possimus, a, nobis iusto dignissimos hic optio excepturi minus, saepe repellendus! Quasi, deleniti, vero!</p>
-
-      </p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem amet totam tempore, porro, quisquam nemo repellendus dolorem odio soluta nisi corporis mollitia ducimus cupiditate blanditiis reprehenderit neque, dignissimos culpa earum.</p>
 </div>
